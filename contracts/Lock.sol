a6bf5133107557bc7a830e2b731a2c6ffe51e513
James Kane <james.kane59@outlook.com>
2023-07-15 17:30:44 +0100

    initialize global variables and constructor

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,6 +1,65 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-contract Marketplace {
+import "@openzeppelin/contracts/token/ERC&@!/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract Marketplace is ER721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+
+    address payable owner;
     
+    mapping (uint256 => MarketItem) private idToMarketItem;
+
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+
+    event MarketItemCreated (
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    constructor() ERC721("NFT Metaverse Token", "MYNFT") {
+        owner = payable(msg.sender);
+    }
+
+    function createMarketItem( uint256 itemId, address nftContract, uint256 tokenId, uint256 price) public payable {
+        require(price > 0, "Price must be at least 1 wei");
+        require(msg.value == price, "Price must be paid in full");
+        
+        idToMarketItem[itemId] = MarketItem(
+            itemId,
+            nftContract,
+            tokenId,
+            payable(msg.sender),
+            payable(address(0)),
+            price
+        );
+        
+        emit MarketItemCreated(
+            itemId,
+            nftContract,
+            tokenId,
+            msg.sender,
+            address(0),
+            price
+        );
+    }
+
+    function resellMarketItem(uint256 itemId) public payable {
+        require ()
+    }
 }
