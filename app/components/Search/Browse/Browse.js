bb3564692227e6f5e3624b6f572ad02d5da6e0ab
James Kane <james.kane59@outlook.com>
2023-10-15 17:19:52 +0100

    manage state with select category in browse component

--- app/components/Search/Browse/Browse.js
+++ app/components/Search/Browse/Browse.js
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client';
+
+import React, { useState, useEffect } from 'react';
 
 // INTERNAL IMPORTS
 import Style from './Browse.module.scss';
@@ -8,6 +10,14 @@ import { AuctionCard } from '../../componentindex';
 import { FaSearch } from 'react-icons/fa';
 
 const Browse = () => {
+	const [selectedCategory, setSelectedCategory] = useState('');
+
+	const handleCategorySelect = (e) => {
+		setSelectedCategory(e.target.innerText);
+	};
+
+	useEffect(() => {}, [selectedCategory]);
+
 	return (
 		<div className={Style.browse}>
 			<div className={Style.browse_search}>
@@ -15,11 +25,21 @@ const Browse = () => {
 				<input type='text' className={Style.browse_search_input} placeholder='Search NFTs' />
 			</div>
 			<div className={Style.browse_wrapper}>
-				<button className={Style.browse_wrapper_genre}>Digital Art</button>
-				<button className={Style.browse_wrapper_genre}>Gaming</button>
-				<button className={Style.browse_wrapper_genre}>Sport</button>
-				<button className={Style.browse_wrapper_genre}>Photography</button>
-				<button className={Style.browse_wrapper_genre}>Music</button>
+				<button className={Style.browse_wrapper_genre} onClick={handleCategorySelect}>
+					Digital Art
+				</button>
+				<button className={Style.browse_wrapper_genre} onClick={handleCategorySelect}>
+					Gaming
+				</button>
+				<button className={Style.browse_wrapper_genre} onClick={handleCategorySelect}>
+					Sport
+				</button>
+				<button className={Style.browse_wrapper_genre} onClick={handleCategorySelect}>
+					Photography
+				</button>
+				<button className={Style.browse_wrapper_genre} onClick={handleCategorySelect}>
+					Music
+				</button>
 			</div>
 			<div className={Style.browse_auctions}>
 				<AuctionCard />
