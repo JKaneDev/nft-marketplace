ef080feaba801b652a2759c6da2946d357511a16
James Kane <james.kane59@outlook.com>
2023-11-28 11:44:57 +0000

    users can withdraw bids if not highest bidder

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -12,8 +12,26 @@ contract Auction is ReentrancyGuard {
 
         mapping(address => uint256) public pendingReturns;
 
+        event Bid(address bidder, uint256 bidAmount, address auctionAddress);
+
         constructor(uint256 _nftId, uint256 _startingPrice, uint256 _currentTimestamp, uint256 _auctionDuration, address _seller) {
                 seller = payable(_seller);
+        }
+
+        function withdraw() public returns (bool) {
+                uint256 amount = pendingReturns[msg.sender];
+
+                // Execute withdrawal if user has funds to withdraw
+                if (amount > 0) {
+                        pendingReturns[msg.sender] = 0;
+
+                        // Alter contract data only if withdrawal is successful
+                        if (!payable(msg.sender).send(amount)) {
+                                pendingReturns[msg.sender] = amount;
+                                return false;
+                        }
+                }
+                return true;
         }
 
         function bid() public payable nonReentrant {
@@ -21,7 +39,13 @@ contract Auction is ReentrancyGuard {
                 require(msg.value > highestBid, 'Bid must be greater than current highest bid');
                 require(highestBidder != msg.sender, 'Bidder is already highest bidder');
 
+                if (highestBid != 0) {
+                        pendingReturns[highestBidder] += highestBid;
+                }
+
                 highestBid = msg.value;
                 highestBidder = payable(msg.sender);
+
+                emit Bid(msg.sender, msg.value, address(this));
         }
 }
